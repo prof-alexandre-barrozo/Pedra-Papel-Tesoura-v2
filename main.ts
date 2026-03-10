d9fe37b7861436ca99e8f87707aac81fceea7147
prof-alexandre-barrozo <prof.eng.alexandre@gmail.com>
2026-03-09 21:38:24 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+let Tremor = 0
+input.onGesture(Gesture.Shake, function () {
+    Tremor = randint(1, 3)
+    if (Tremor == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else if (Tremor == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+})
